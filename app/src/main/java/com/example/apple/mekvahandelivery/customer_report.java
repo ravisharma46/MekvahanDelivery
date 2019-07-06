4ed38f57a38c78aa00a4c7ca5539617a593d5cb9
ravisharma46 <ravi.ravi.sharma46@gmail.com>
2019-07-06 21:02:26 +0530

    update customer report

--- app/src/main/java/com/example/apple/mekvahandelivery/customer_report.java
+++ app/src/main/java/com/example/apple/mekvahandelivery/customer_report.java
@@ -1,14 +1,27 @@
 package com.example.apple.mekvahandelivery;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class customer_report extends AppCompatActivity {
+
+
+    FrameLayout car,bike;
+    ImageView car_image,bike_image;
+    TextView tvbike,tvcar,document;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +37,80 @@ public class customer_report extends AppCompatActivity {
         getSupportActionBar().setHomeAsUpIndicator(upArrow);
 
 
+        car=(FrameLayout) findViewById(R.id.frame_2);
+        bike=(FrameLayout) findViewById(R.id.frame_1);
+        car_image=(ImageView) findViewById(R.id.car_image);
+        bike_image=(ImageView) findViewById(R.id.bike_image);
+        tvbike=(TextView) findViewById(R.id.tvbike);
+        tvcar=(TextView)findViewById(R.id.tvcar);
+        document=(TextView)findViewById(R.id.tvDocument);
+
+        loadBikeFragment();
+        load_bike();
+
+        car.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadCarFragment();
+                load_car();
+
+
+
+            }
+        });
+
+        bike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               loadBikeFragment();
+               load_bike();
+
+
+            }
+        });
+
+
     }
+
+
+    private void load_bike(){
+        bike.setBackgroundResource(R.color.chart_deep_red);
+        tvbike.setTextColor(Color.WHITE);
+        bike_image.setImageResource(R.drawable.bike_white);
+
+        car.setBackgroundResource(R.color.white);
+        tvcar.setTextColor(Color.BLACK);
+        car_image.setImageResource(R.drawable.carblack);
+        document.setText("Bike document");
+    }
+    private void load_car(){
+        bike.setBackgroundResource(R.color.white);
+        tvbike.setTextColor(Color.BLACK);
+        bike_image.setImageResource(R.drawable.bike_black);
+
+        car.setBackgroundResource(R.color.chart_deep_red);
+        tvcar.setTextColor(Color.WHITE);
+        car_image.setImageResource(R.drawable.car_white);
+        document.setText("Car document");
+    }
+
+
+    private  void loadCarFragment(){
+        car_fragment c_Fragment=new car_fragment();
+        android.support.v4.app.FragmentManager manager= getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.frameLayout,
+                c_Fragment,
+                c_Fragment.getTag()).commit();
+    }
+
+    private void loadBikeFragment(){
+        bike_fragment b_Fragment=new bike_fragment();
+        android.support.v4.app.FragmentManager manager= getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.frameLayout,
+                b_Fragment,
+                b_Fragment.getTag()).commit();
+    }
+
+
+
 }
